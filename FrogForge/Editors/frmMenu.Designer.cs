fced4a6a81dd5c7258fff3aad07f9eb4b821588e
GurLad <40135047+GurLad@users.noreply.github.com>
2022-01-23 18:01:52 +0200

    Date
    Oops

--- FrogForge/Editors/frmMenu.Designer.cs
+++ FrogForge/Editors/frmMenu.Designer.cs
@@ -110,7 +110,7 @@
             this.lblVersion.Name = "lblVersion";
             this.lblVersion.Size = new System.Drawing.Size(289, 11);
             this.lblVersion.TabIndex = 4;
-            this.lblVersion.Text = "V.0.5.8.4 09.01.2021";
+            this.lblVersion.Text = "V.0.5.8.4 09.01.2022";
             this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // btnClassEditor
